4991de2d5018bc7a6959e6c546c13886df1cc969
Faddle Badiane <faddle.badiane@cygnvs.com>
2024-01-21 23:53:08 +0000

    dynamo table added to yaml and some basic query code

--- hello-world/app.ts
+++ hello-world/app.ts
@@ -10,9 +10,48 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
  *
  */
 
+// Create clients and set shared const values outside of the handler.
+
+// Create a DocumentClient that represents the query to add an item
+import dynamodb from 'aws-sdk/clients/dynamodb';
+const docClient = new dynamodb.DocumentClient();
+
+// Get the DynamoDB table name from environment variables
+const tableName: string = process.env.CARDS_TABLE;
+
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     let response: APIGatewayProxyResult;
     try {
+        try {
+            const params = {
+                TableName: tableName,
+                IndexName: 'type-index',
+                KeyConditionExpression: `#pk = :pk`,
+                ExpressionAttributeNames: {
+                    '#pk': 'type',
+                },
+                ExpressionAttributeValues: {
+                    ':pk': 'Credit',
+                    ':deleted': 0,
+                },
+            };
+
+            const result = await docClient.query(params).promise();
+
+            response = {
+                statusCode: 200,
+                body: JSON.stringify(result),
+            };
+        } catch (ResourceNotFoundException) {
+            response = {
+                statusCode: 404,
+                body: 'Unable to call DynamoDB. Table resource not found.',
+            };
+        }
+
+        // All log statements are written to CloudWatch
+        console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+
         response = {
             statusCode: 200,
             body: JSON.stringify({
